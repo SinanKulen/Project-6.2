8aed098b7aa45078eafa9b90ca48755e74e8ae16
SinanKulen <sinan.kulen@gmail.com>
2021-07-13 13:30:43 +0300

    Initial Commit

---
+++ Project 6.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 6.2
+//
+//  Created by Sinan Kulen on 13.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
